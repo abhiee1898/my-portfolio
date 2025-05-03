10718ca4fba1a7f5ae641fc5b1c4ee50e9f6c249
abhiee1898 <cool.abhi1898@gmail.com>
2025-05-03 23:01:06 +0530

    7th commit

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -15,7 +15,7 @@ interface Props {
   }
 }
 
-export async function generateStaticParams(): Promise<{ slug: string }[]> {
+export async function generateStaticParams() {
   const blogDir = path.join(process.cwd(), 'src/content/blogs')
   const files = fs.readdirSync(blogDir)
 
@@ -24,7 +24,7 @@ export async function generateStaticParams(): Promise<{ slug: string }[]> {
   }))
 }
 
-export default async function BlogPage({ params }: {params: {slug : string}}) {
+export default async function BlogPage({ params }: Props) {
   const filePath = path.join(process.cwd(), 'src/content/blogs', `${params.slug}.md`)
   const fileContent = fs.readFileSync(filePath, 'utf8')
   const { data, content } = matter(fileContent)
